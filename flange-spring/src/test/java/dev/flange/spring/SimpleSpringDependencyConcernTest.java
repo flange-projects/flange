c22f3de93ec9c8c347322cab334642dd77bb5c87
Garret Wilson <garret@globalmentor.com>
2023-10-09 18:40:09 -0300

    FLANGE-2: Enabled Spring constructor dependency injection.

--- flange-spring/src/test/java/dev/flange/spring/SimpleSpringDependencyConcernTest.java
+++ flange-spring/src/test/java/dev/flange/spring/SimpleSpringDependencyConcernTest.java
@@ -16,8 +16,11 @@
 
 package dev.flange.spring;
 
+import static java.util.Objects.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
+
+import javax.annotation.*;
 
 import org.junit.jupiter.api.*;
 
@@ -37,4 +40,34 @@ public class SimpleSpringDependencyConcernTest {
 		assertThat(dependencyConcern.getDependencyInstanceByType(Appendable.class), instanceOf(StringBuilder.class));
 	}
 
+	/** Tests registering a type and then looking it up using constructor injection. */
+	@Test
+	void testRegisterLookupConstructorInjection() {
+		final DependencyConcern dependencyConcern = new SimpleSpringDependencyConcern();
+		dependencyConcern.registerDependency(StringBuilder.class);
+		dependencyConcern.registerDependency(Appender.class);
+		final Appender appender = dependencyConcern.getDependencyInstanceByType(Appender.class);
+		assertThat(appender.getAppendable(), instanceOf(StringBuilder.class));
+	}
+
+	/** Class for testing constructor injection. */
+	private static class Appender {
+
+		private final Appendable appendable;
+
+		/** @return The appendable instance set by the constructor. */
+		public Appendable getAppendable() {
+			return appendable;
+		}
+
+		/**
+		 * Appendable constructor.
+		 * @param appendable The appendable to use.
+		 */
+		@SuppressWarnings("unused") //used by dependency injection
+		public Appender(@Nonnull final Appendable appendable) {
+			this.appendable = requireNonNull(appendable);
+		}
+
+	}
 }
